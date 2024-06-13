4499141bfea6727e345255bd691d872ea1ab1165
wanghaisheng <edwin.uestc@gmail.com>
2024-06-14 02:59:01 +0800

    move cors parts to end

--- api/index.py
+++ api/index.py
@@ -14,15 +14,6 @@ def healthchecker():
     return {"status": "success", "message": "Integrate FastAPI Framework with Next.js"}
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 class TodoCreate(BaseModel):
     title: str
 
@@ -146,3 +137,17 @@ async def getAhrefKD(keyword: str):
     #     print(kds)
 
     return {"keyword": keyword, "kd": kd, "des": kds}
+
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
