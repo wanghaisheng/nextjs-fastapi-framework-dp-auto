e3cbddfac6ecc97aa8e2933af749b6382286265f
wanghaisheng <edwin.uestc@gmail.com>
2024-06-14 22:43:10 +0800

    borrow from other

--- Makefile
+++
@@ -1,9 +0,0 @@
-commands:
-	python3 -m venv venv
-	source venv/bin/activate
-
-nextjs-packages:
-	pnpm add concurrently zustand
-
-py-modules:
-	pip install fastapi 'uvicorn[standard]'
--- api/index.py
+++ api/index.py
@@ -1,96 +1,139 @@
-from typing import Union
-
-from fastapi import FastAPI
+import secure
+import uvicorn
+from api.config import settings
+from api.dependencies import validate_token
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from dotenv import load_dotenv
+import os
+from .cf_bypass import CloudflareBypass
+import os
 
-app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+# load_dotenv()
 
+app = FastAPI()
 
-@app.get("/api/healthchecker")
-def healthchecker():
-    return {"status": "success", "message": "Integrate FastAPI Framework with Next.js"}
+csp = secure.ContentSecurityPolicy().default_src("'self'").frame_ancestors("'none'")
+hsts = secure.StrictTransportSecurity().max_age(31536000).include_subdomains()
+referrer = secure.ReferrerPolicy().no_referrer()
+cache_value = secure.CacheControl().no_cache().no_store().max_age(0).must_revalidate()
+x_frame_options = secure.XFrameOptions().deny()
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+secure_headers = secure.Secure(
+    csp=csp,
+    hsts=hsts,
+    referrer=referrer,
+    cache=cache_value,
+    xfo=x_frame_options,
 )
 
 
-class TodoCreate(BaseModel):
-    title: str
+@app.middleware("http")
+async def set_secure_headers(request, call_next):
+    response = await call_next(request)
+    secure_headers.framework.fastapi(response)
+    return response
 
 
-class TodoUpdate(BaseModel):
-    title: Union[str, None] = None
-    completed: Union[bool, None] = None
+origins = [
+    "*",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "https://nextjs-fastapi-framework-dp-auto.vercel.app/",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET"],
+    allow_headers=["Authorization", "Content-Type"],
+    max_age=86400,
+)
 
 
-class TodoItem(BaseModel):
-    id: int
-    title: str
-    completed: bool
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    message = str(exc.detail)
+
+    return JSONResponse({"message": message}, status_code=exc.status_code)
 
 
-# Define the TodoItem model
-class TodoItem(BaseModel):
-    id: int
-    title: str
-    completed: bool
+@app.get("/api/messages/public")
+def public():
+    return {"text": "This is a public message."}
 
 
-# In-memory storage for todo items
-todos = []
-
-# Route to create a new todo item
-
-
-@app.post("/api/todos")
-def create_todo_item(todo: TodoCreate):
-    new_todo = TodoItem(id=len(todos) + 1, title=todo.title, completed=False)
-    todos.append(new_todo)
-    return new_todo
-
-# Route to get all todo items
+@app.get("/api/messages/protected", dependencies=[Depends(validate_token)])
+def protected():
+    return {"text": "This is a protected message."}
 
 
-@app.get("/api/todos")
-def get_all_todo_items():
-    return todos
-
-# Route to get a specific todo item by ID
+@app.get("/api/python")
+def hello_world():
+    return {"message": "Hello World"}
 
 
-@app.get("/api/todos/{todo_id}")
-def get_todo_item(todo_id: int):
-    for todo in todos:
-        if todo.id == todo_id:
-            return todo
-    return {"error": "Todo item not found"}
-
-# Route to update a specific todo item by ID
+@app.get("/api/auth/me")
+def auth_me():
+    # print("><>>>>>>")
+    # print(value)
+    return None
 
 
-@app.patch("/api/todos/{todo_id}")
-def update_todo_item(todo_id: int, todo: TodoUpdate):
-    for todo_item in todos:
-        if todo_item.id == todo_id:
-            todo_item.title = todo.title if todo.title is not None else todo_item.title
-            todo_item.completed = todo.completed if todo.completed is not None else todo_item.completed
-            return todo_item
-    return {"error": "Todo item not found"}
+# Your route now expects JSON body with 'keywords' field
+@app.post("/api/ahref/kd/")
+async def getAhrefKD(keyword: str = Body(...)):  # Use Body to get the
+    if keyword:
+        path = "/tmp/chromium"
+        cloudflare_bypass = None
+        # Try each path in sequence until a valid one is found
 
-# Route to delete a specific todo item by ID
+        # Check if the path exists
+        if os.path.exists(path):
+            print("tmp is found")
+            # List all files and directories in the path
+            files_and_dirs = os.listdir(path)
 
+            # Filter out directories and only list files
+            files = [f for f in files_and_dirs if os.path.isfile(os.path.join(path, f))]
 
-@app.delete("/api/todos/{todo_id}")
-def delete_todo_item(todo_id: int):
-    for i, todo_item in enumerate(todos):
-        if todo_item.id == todo_id:
-            del todos[i]
-            return {"message": "Todo item deleted"}
-    return {"error": "Todo item not found"}
+            # Print all files
+            for file in files:
+                print(file)
+            cloudflare_bypass = CloudflareBypass(browser_path=path)
+
+        else:
+            print("The path does not exist")
+            cloudflare_bypass = CloudflareBypass(browser_path=None)
+
+        # co = ChromiumOptions().set_browser_path(path).auto_port()
+        # page1 = ChromiumPage(co)
+        page1 = cloudflare_bypass.driver
+        url = "https://ahrefs.com/keyword-difficulty/"
+        if "," in keyword:
+            keywords = keyword.split(",")
+        else:
+            keywords = [keyword]
+        datas = []
+        if keyword in keywords:
+            page1.get(url)
+            # keyword = "remini.ai"
+            page1.ele("@placeholder=Enter keyword").input(keyword)
+
+            # 点击登录按钮
+            page1.ele("text=Check keyword").click()
+            cookies = cloudflare_bypass.bypass(url)
+
+            kd = page1.ele(".css-16bvajg-chartValue").text
+
+            kds = page1.ele(".css-1wi5h2-row css-1crciv5 css-6rbp9c").text
+            #     print(kd)
+            #     print(kds)
+            data = {"keyword": keyword, "kd": kd, "des": kds}
+            datas.append(data)
+
+            return data
+    else:
+        return []
