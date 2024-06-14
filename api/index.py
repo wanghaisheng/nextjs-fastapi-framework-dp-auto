9c202787f2607321870c0f395b574b580bea235e
wanghaisheng <edwin.uestc@gmail.com>
2024-06-14 11:33:55 +0800

    loose origins

--- api/index.py
+++ api/index.py
@@ -4,7 +4,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import os
-from .cf_bypass import CloudflareBypass
+
+# from .cf_bypass import CloudflareBypass
 
 app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
@@ -112,41 +113,42 @@ def delete_todo_item(todo_id: int):
 async def getAhrefKD(keyword: str):
     path = "/tmp/chromium"
 
-    # Try each path in sequence until a valid one is found
+    # # Try each path in sequence until a valid one is found
 
-    # Check if the path exists
-    if os.path.exists(path):
-        print("tmp is found")
-        # List all files and directories in the path
-        files_and_dirs = os.listdir(path)
+    # # Check if the path exists
+    # if os.path.exists(path):
+    #     print("tmp is found")
+    #     # List all files and directories in the path
+    #     files_and_dirs = os.listdir(path)
 
-        # Filter out directories and only list files
-        files = [f for f in files_and_dirs if os.path.isfile(os.path.join(path, f))]
+    #     # Filter out directories and only list files
+    #     files = [f for f in files_and_dirs if os.path.isfile(os.path.join(path, f))]
 
-        # Print all files
-        for file in files:
-            print(file)
-    else:
-        print("The path does not exist")
+    #     # Print all files
+    #     for file in files:
+    #         print(file)
+    # else:
+    #     print("The path does not exist")
 
-    # co = ChromiumOptions().set_browser_path(path).auto_port()
-    # page1 = ChromiumPage(co)
-    cloudflare_bypass = CloudflareBypass(browser_path=path)
-    page1 = cloudflare_bypass.driver
-    url = "https://ahrefs.com/keyword-difficulty/"
+    # # co = ChromiumOptions().set_browser_path(path).auto_port()
+    # # page1 = ChromiumPage(co)
+    # cloudflare_bypass = CloudflareBypass(browser_path=path)
+    # page1 = cloudflare_bypass.driver
+    # url = "https://ahrefs.com/keyword-difficulty/"
 
-    page1.get(url)
-    # keyword = "remini.ai"
-    page1.ele("@placeholder=Enter keyword").input(keyword)
+    # page1.get(url)
+    # # keyword = "remini.ai"
+    # page1.ele("@placeholder=Enter keyword").input(keyword)
 
-    # 点击登录按钮
-    page1.ele("text=Check keyword").click()
-    cookies = cloudflare_bypass.bypass(url)
+    # # 点击登录按钮
+    # page1.ele("text=Check keyword").click()
+    # cookies = cloudflare_bypass.bypass(url)
 
-    kd = page1.ele(".css-16bvajg-chartValue").text
+    # kd = page1.ele(".css-16bvajg-chartValue").text
 
-    kds = page1.ele(".css-1wi5h2-row css-1crciv5 css-6rbp9c").text
-    #     print(kd)
-    #     print(kds)
+    # kds = page1.ele(".css-1wi5h2-row css-1crciv5 css-6rbp9c").text
+    # #     print(kd)
+    # #     print(kds)
 
-    return {"keyword": keyword, "kd": kd, "des": kds}
+    # return {"keyword": keyword, "kd": kd, "des": kds}
+    return {}
